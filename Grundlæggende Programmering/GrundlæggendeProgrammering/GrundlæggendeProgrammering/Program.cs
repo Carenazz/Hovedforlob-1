dca901e272f7115bd4dee79c3600668e39465fa4
Carenazz <73272083+Carenazz@users.noreply.github.com>
2021-08-03 08:34:39 +0200

    Tilføjelse af Grundlæggende Programmering
    Tilføjet grundlæggende programmerings mappen + visual studio for at få mappen vist.

---
+++ Grundlæggende Programmering/GrundlæggendeProgrammering/GrundlæggendeProgrammering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GrundlæggendeProgrammering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
